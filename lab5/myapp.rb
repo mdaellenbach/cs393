30f4a3221e51c7208881b3ed1b968bba665e6c79
mdaellenbach <mdaellenbach@lclark.edu>
2014-10-24 18:18:05 -0700

    Set up app to put on Heroku

---
+++ lab5/myapp.rb
@@ -0,0 +1,28 @@
+#myapp.rb
+
+#use bundler
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require
+require './models/TodoItem'
+
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  ActiveRecord::Base.establish_connection(
+    :adapter  => 'sqlite3',
+    :database => 'db/development.db',
+    :encoding => 'utf8'
+  )
+end
+
+#the fact that there's nothing after the '/' means that it's not going to a different webpage
+get '/' do
+  @tasks = TodoItem.all
+  erb :list
+end
+
+post '/' do
+  TodoItem.create(params)
+  redirect '/'
+end
