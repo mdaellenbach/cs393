7ef867d452df3e246785f023fa919549185f8d06
mdaellenbach <mdaellenbach@lclark.edu>
2014-10-27 10:08:16 -0700

    Saving progress to work on lab 5

--- lab1/db/migrate/20141016192916_create_todo_items.rb
+++
@@ -1,8 +0,0 @@
-class CreateTodoItems < ActiveRecord::Migration
-  def change
-    create_table :todo_items do |t|
-      t.string :task
-      t.string :date
-    end
-  end
-end
--- lab1/myapp.rb
+++ lab1/myapp.rb
@@ -1,24 +1,18 @@
-#myapp.rb
-
-#use bundler
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require
-require './models/TodoItem'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => 'db/development.db',
-  :encoding => 'utf8'
-)
-
-#the fact that there's nothing after the '/' means that it's not going to a different webpage
 get '/' do
-  @tasks = TodoItem.all
-  erb :list
+  erb :index
+end
+
+require 'json' #nice output formatting
+post '/' do
+  JSON.pretty_generate params
 end
 
 post '/' do
-  TodoItem.create(params)
-  redirect '/'
+    File.open("todo.txt", "a") do |file|
+    file.puts "#{params[:task]} - #{params[:date]}"
+  end
 end
